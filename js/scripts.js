fe4e6c3e479e42e685598bc120df25b408eb0aff
Irene Truong <irenesis123@gmail.com>
2020-07-03 22:23:06 -0400

    some refactoring

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,17 @@ const timer = document.getElementById("timer");
 const startBtn = document.getElementById("start-btn");
 const newGameBtn = document.getElementById("new-game-btn");
 const scoreDisplay = document.getElementById("score");
-let startModal = document.querySelector(".start-modal");
-let currentGame = document.querySelector(".current-game");
+const startModal = document.querySelector(".start-modal");
+const currentGame = document.querySelector(".current-game");
+const countdownTimerDOM = document.querySelector(".countdown");
+const finishedModal = document.querySelector(".finished-modal");
 
-let startTime;
+// Global Variables
+// let startTime;
+let time = 21;
+let score = 0;
+let countdownTimer = 4;
+let isPlaying = false;
 
 getQuote = () => {
   return fetch(`https://api.quotable.io/random`)
@@ -16,38 +23,70 @@ getQuote = () => {
     .then((data) => data.content);
 };
 
-// returns time value
-getTimerTime = () => {
-  return Math.floor((new Date() - startTime) / 1000);
-};
-
-startTimer = () => {
-  timer.innerText = 0;
-  startTime = new Date();
-  setInterval(() => {
-    timer.innerText = "Seconds: " + getTimerTime();
-  }, 1000);
-};
-
 async function newQuote() {
   const quote = await getQuote();
   quoteDisplay.innerText = "";
   //   splits the quote per letter
   quote.split("").forEach((character) => {
-    //   has to be span since p and div are block element
     const quoteParagraph = document.createElement("span");
     quoteParagraph.innerText = character;
     quoteDisplay.appendChild(quoteParagraph);
   });
   quoteInput.value = null;
-
-  startModal.style.display = "none";
-  currentGame.style.display = "block";
-
-  startTimer();
 }
 
-// initialize Game
-startBtn.addEventListener("click", newQuote);
+init = () => {
+  isPlaying = true;
+  setInterval(countdown, 1000);
+  newQuote();
+  console.log("check", quoteDisplay.innerText);
 
-newGameBtn.addEventListener("click", newQuote);
+  quoteInput.addEventListener("input", checkMatch);
+  // check game status
+  setInterval(checkStatus, 50);
+};
+
+countdown = () => {
+  if (countdownTimer > 0) {
+    countdownTimer--;
+    // after countdown, start actual game countdown
+  } else if (countdownTimer === 0) {
+    if (time > 0) {
+      startModal.style.display = "none";
+      currentGame.style.display = "block";
+      time--;
+    } else if (time === 0) {
+      isPlaying = false;
+      currentGame.style.display = "none";
+      finishedModal.style.display = "block";
+    }
+    timer.innerHTML = time;
+  }
+  countdownTimerDOM.innerHTML = countdownTimer;
+};
+
+checkStatus = () => {
+  if (!isPlaying && time === 0) {
+  }
+};
+
+checkMatch = () => {
+  if (matchWords()) {
+  }
+};
+
+matchWords = () => {
+  if (quoteInput.value === quoteDisplay.innerText) {
+    console.log("correct");
+    score++;
+    newQuote();
+    return true;
+  } else {
+    console.log("wrong");
+    return false;
+  }
+};
+
+// Start/Restart Game
+startBtn.addEventListener("click", init);
+// newGameBtn.addEventListener("click", location.reload());
