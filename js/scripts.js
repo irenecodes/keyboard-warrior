0c33a538fb8df8b4d8e50832aa59e2122e60c66b
Irene Truong <irenesis123@gmail.com>
2020-07-04 03:28:17 -0400

    almost MVP

--- js/scripts.js
+++ js/scripts.js
@@ -9,13 +9,27 @@ const startModal = document.querySelector(".start-modal");
 const currentGame = document.querySelector(".current-game");
 const countdownTimerDOM = document.querySelector(".countdown");
 const finishedModal = document.querySelector(".finished-modal");
+const overallTimer = document.getElementById("overall-timer");
 
 // Global Variables
-// let startTime;
-let time = 21;
+//use Date.now() because setInt and setTimeout are arbitrary
+let startTime = Date.now();
+let newTime;
+let time = 5; //21;
 let score = 0;
 let countdownTimer = 4;
 let isPlaying = false;
+
+// returns overall time value
+watchTime = () => {
+  if (isPlaying) {
+    setInterval(() => {
+      newTime = Math.floor((Date.now() - startTime) / 1000);
+    }, 1000);
+  }
+  // rid of countdown timer
+  overallTimer.innerText = newTime - 3;
+};
 
 getQuote = () => {
   return fetch(`https://api.quotable.io/random`)
@@ -35,12 +49,17 @@ async function newQuote() {
   quoteInput.value = null;
 }
 
+checkStatus = () => {
+  if (!isPlaying) {
+    document.getElementById("quoteInput").disabled = true;
+  } else {
+    document.getElementById("quoteInput").disabled = false;
+  }
+};
+
 init = () => {
-  isPlaying = true;
   setInterval(countdown, 1000);
   newQuote();
-  console.log("check", quoteDisplay.innerText);
-
   quoteInput.addEventListener("input", checkMatch);
   // check game status
   setInterval(checkStatus, 50);
@@ -55,8 +74,9 @@ countdown = () => {
       startModal.style.display = "none";
       currentGame.style.display = "block";
       time--;
+      isPlaying = true;
+      watchTime();
     } else if (time === 0) {
-      isPlaying = false;
       currentGame.style.display = "none";
       finishedModal.style.display = "block";
     }
@@ -65,21 +85,19 @@ countdown = () => {
   countdownTimerDOM.innerHTML = countdownTimer;
 };
 
-checkStatus = () => {
-  if (!isPlaying && time === 0) {
-  }
-};
-
 checkMatch = () => {
   if (matchWords()) {
+    score++;
+    newQuote();
+    time = 11;
   }
+  scoreDisplay.innerHTML = score;
 };
 
+// returns boolean
 matchWords = () => {
   if (quoteInput.value === quoteDisplay.innerText) {
     console.log("correct");
-    score++;
-    newQuote();
     return true;
   } else {
     console.log("wrong");
@@ -87,6 +105,5 @@ matchWords = () => {
   }
 };
 
-// Start/Restart Game
+// Start Game
 startBtn.addEventListener("click", init);
-// newGameBtn.addEventListener("click", location.reload());
